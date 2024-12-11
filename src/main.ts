a0758d9e1b78eb0b3e2b092fc339e3011a863571
Muhammad-Abid11 <abidkhan11845@gmail.com>
2024-12-11 13:02:06 +0500

    Revert "feat: add validation feature"
    This reverts commit 718f42c61571786bad3fee1b77802dbec91396ea.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   // Enable global validation
-   app.useGlobalPipes(new ValidationPipe(
-    {
-      whitelist: true, // Remove extra properties not in the DTO
-      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
-      transform: true, // Automatically transform payloads to match DTO types
-    }
-   ));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
@@ -43,11 +34,5 @@ In your.module.ts, register the schema and add the service and controller
 npm run start
 npm run start:dev
 
-Validation
-  To add validation to your NestJS project, you can use the class-validator and class-transformer libraries,
-  which work well with NestJS. Here's how to integrate validation for your CRUD operations:
-
-  npm install class-validator class-transformer
-  Create DTOs (Data Transfer Objects)
 
 */
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,43 +1,32 @@
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { Todo } from './todo.schema';
-import { CreateTodoDto, UpdateTodoDto } from './todo.dto';
 
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
-/*   @Post()
+  @Post()
   create(@Body() data: Partial<Todo>) {
     return this.todoService.create(data);
-  } */
-
-  @Post()//http://localhost:3000/todo
-  create(@Body() createYourDto: CreateTodoDto) {
-    return this.todoService.create(createYourDto);
   }
 
-  @Get()//http://localhost:3000/todo
+  @Get()
   findAll() {
     return this.todoService.findAll();
   }
 
-  @Get(':id')//http://localhost:3000/todo/<id>
+  @Get(':id')
   findOne(@Param('id') id: string) {
     return this.todoService.findOne(id);
   }
 
-/*   @Put(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() data: Partial<Todo>) {
     return this.todoService.update(id, data);
-  } */
-  
-  @Put(':id')//http://localhost:3000/todo/<id>
-  update(@Param('id') id: string, @Body() updateYourDto: UpdateTodoDto) {
-    return this.todoService.update(id, updateYourDto);
   }
 
-  @Delete(':id')//http://localhost:3000/todo/<id>
+  @Delete(':id')
   delete(@Param('id') id: string) {
     return this.todoService.delete(id);
   }
